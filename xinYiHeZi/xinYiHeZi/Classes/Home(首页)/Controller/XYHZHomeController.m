3f9170575386041381724d3ba8f33c285f10bddc
sulcan <lixinyu@lxy.local>
2016-03-29 12:55:24 +0800

    test1

--- xinYiHeZi/xinYiHeZi/Classes/Home(首页)/Controller/XYHZHomeController.m
+++ xinYiHeZi/xinYiHeZi/Classes/Home(首页)/Controller/XYHZHomeController.m
@@ -22,7 +22,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    NSLog(@"=========================kk======================");
+    NSLog(@"=====================古城======================");
 }
 
 - (void)didReceiveMemoryWarning {
